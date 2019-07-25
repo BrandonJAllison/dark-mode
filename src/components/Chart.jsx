63c95ca8a53a78239e69077c3b39dc06da8db798
Brandon Allison <brandona8128@outlook.com>
2019-07-25 12:16:05 -0400

    works for mvp

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -28,8 +28,8 @@ const Chart = ({ sparklineData }) => {
 
   return (
     <LineChart width={1100} height={300} data={formattedData}>
-      <Line type="monotone" dataKey="value" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <Line type="monotone" dataKey="value" stroke="#6049e6" />
+      <CartesianGrid stroke="#57b7bd" strokeDasharray="5 5" />
       <XAxis dataKey="date" interval={3} />
       <YAxis />
       <Tooltip />
